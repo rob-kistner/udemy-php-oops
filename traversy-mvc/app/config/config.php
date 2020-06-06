cf9e344251d95753f864766ba44571e49053cfac
rob-kistner <robkistner@mac.com>
2020-06-05 22:17:29 -0500

    archiving

--- traversy-mvc/app/config/config.php
+++ traversy-mvc/app/config/config.php
@@ -9,13 +9,13 @@ define('DB_NAME', 'traversymvc');
 
 // app root
 
-// ----------------------------------------
-// parent folder of current file
-//     echo dirname(__FILE__);
-//
-// parent of parent folder of current file
-// which will be the app root
-// ----------------------------------------
+/**
+ * parent folder of current file
+ *     echo dirname(__FILE__);
+ *
+ * parent of parent folder of current file
+ * which will be the app root
+ */
 define( 'APPROOT',  dirname(dirname(__FILE__)) );
 
 // URL root
